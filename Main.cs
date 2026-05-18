9470c5c4f3b5ba8a716cf18352fafa984aad8cc3
Ксения Шмыкова <sh_ksenya@MacBook-Air-Ksenia.local>
2026-05-18 21:30:17 +0300

    Initial commit

---
+++ Main.cs
@@ -0,0 +1,28 @@
+﻿
+using AlternateLife.RageMP.Net.Scripting;
+using AlternateLife.RageMP.Net.EventArgs;
+
+namespace MyRageMPServer;
+
+public class Main
+{
+    public Main()
+    {
+        MP.Logger.Info("=== Сервер запущен! ===");
+
+        MP.Events.PlayerJoin += OnPlayerJoin;
+        MP.Events.PlayerQuit += OnPlayerQuit;
+    }
+
+    private async System.Threading.Tasks.Task OnPlayerJoin(object sender, PlayerEventArgs e)
+    {
+        var name = await e.Player.GetNameAsync();
+        await e.Player.OutputChatBoxAsync($"Добро пожаловать, {name}!");
+        MP.Logger.Info($"{name} зашёл на сервер!");
+    }
+
+    private void OnPlayerQuit(object sender, PlayerQuitEventArgs e)
+    {
+        MP.Logger.Info($"Игрок вышел: {e.Reason}");
+    }
+}
